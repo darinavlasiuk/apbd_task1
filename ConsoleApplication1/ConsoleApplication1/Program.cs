ad65cf3f1939df74edf7a5ee3e04cce6a1c54544
Daryna Vlasiuk <s26878@pjwstk.edu.pl>
2024-03-19 21:35:09 +0100

    func find average on feature-average branch created

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,5 +10,19 @@ namespace ConsoleApplication1
       Console.WriteLine("World");
       Console.WriteLine("Hey");
     }
+    
+    static double findAvg(int[] arr)
+    {
+      double avg = 0.0;
+      int sum = 0;
+      for (int i = 0; i < arr.Length; ++i)
+      {
+        sum += arr[i];
+      }
+
+      avg = (double)sum / arr.Length;
+      return avg;
+    }
   }
+  
 }
